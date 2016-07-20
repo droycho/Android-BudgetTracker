b7bbf12342a9666d8f94eac0d018bcd80d9a612a
DJ Roychoudhury and John Klein <john@>
2016-07-20 16:32:13 -0700

    login authenticates properly, need to navigate to homeActivity.

--- app/src/main/java/com/epicodus/budgettracker/ui/HomeActivity.java
+++ app/src/main/java/com/epicodus/budgettracker/ui/HomeActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +18,7 @@ import com.epicodus.budgettracker.adapters.FirebaseTransactionListAdapter;
 import com.epicodus.budgettracker.adapters.FirebaseTransactionViewHolder;
 import com.epicodus.budgettracker.adapters.TransactionListAdapter;
 import com.epicodus.budgettracker.models.Transaction;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
@@ -32,5 +36,29 @@ public class  HomeActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            Log.d("Menu: ", "Logout selected");
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 }
